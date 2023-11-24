5a988f45a09a19be2af86f6fad7628ef0205cc3b
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-24 21:41:28 +0000

    segunda django

--- django-segunda_clase/tienda_app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import Tienda
-
-class TiendaSerializer(ModelSerializer):
-    class Meta:
-        model = Tienda
-        fields = "__all__"
--- django-segunda_clase/tiendas_manager/views.py
+++
@@ -1,6 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class Index(TemplateView):
-    extra_context = {"comision": "23508"}
-    template_name = "index.html"
